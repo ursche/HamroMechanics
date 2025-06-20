ece58f60dae00adc19902017c5ec56e474757b89
ursche <urjalapariyar@gmail.com>
2025-06-20 13:28:23 +0545

    user login

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,17 @@
-// app/index.tsx
-import React from 'react';
-import { View, Text } from 'react-native';
 import { Link } from 'expo-router';
+import React from 'react';
+import { Text, View } from 'react-native';
 
 export default function WelcomeScreen() {
   return (
-    <View style={{ flex:1, justifyContent:'center', alignItems:'center' }}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Welcome to our Auto Mobile service. We need a logo now</Text>
-      {/* Link to login page */}
-      <Link href="/login" style={{ marginTop: 20, fontSize: 18, color: 'blue' }}>
-        Go to Login
+
+      {/* Navigation to login screen */}
+      <Link href="/login" asChild>
+        <Text style={{ marginTop: 20, fontSize: 18, color: 'blue' }}>
+          Go to Login
+        </Text>
       </Link>
     </View>
   );
