2af32f243ff940e9deaf22c9d6ef9150ae2fd219
NickName-AM <maharjanebik061@gmail.com>
2025-06-28 20:41:36 +0545

    haversine formula implementation

--- components/Map.tsx
+++ components/Map.tsx
@@ -22,6 +22,27 @@ type MechanicInfo = {
     rating: number;
 }
 
+// Get Distance in km
+function getDistance(lat1: number, lon1: number, lat2: number, lon2: number): number {
+    const R = 6371; // Radius of the Earth in km
+    const dLat = deg2rad(lat2 - lat1);
+    const dLon = deg2rad(lon2 - lon1);
+    const a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(deg2rad(lat1)) *
+            Math.cos(deg2rad(lat2)) *
+            Math.sin(dLon / 2) *
+            Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return R * c;
+}
+
+function deg2rad(deg: number): number {
+    return deg * (Math.PI / 180);
+}
+
+
+
 export default function Map(){
     const [location, setLocation] = useState<LocationCoords | null>(null);
     const [markerPressed, setMarkerPressed] = useState(false);
@@ -115,6 +136,16 @@ export default function Map(){
                     <Text>Latitude: {chosenMechanic?.location!.latitude.toFixed(6)}</Text>
                     <Text>Longitude: {chosenMechanic?.location!.longitude.toFixed(6)}</Text>
                     <Text>Rating: {chosenMechanic?.rating}</Text>
+                    <Text>
+                        Distance: {' '}
+                        {chosenMechanic?.location && location ? getDistance(
+                            location.latitude,
+                            location.longitude,
+                            chosenMechanic.location.latitude,
+                            chosenMechanic.location.longitude,
+                        ).toFixed(2)
+                    : '...'}{' '}
+                    </Text>
 
                     <TouchableOpacity onPress={closeBottomSheet} style={styles.closeButton}>
                         <Text style={styles.closeButtonText}>Close</Text>
