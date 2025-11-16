807e1d46f12b59600cd3dd0ae2bf0e88004a0027
ursche <urjalapariyar@gmail.com>
2025-11-16 15:32:43 +0545

    clean merge

--- backend/mechanics/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from mechanics.models import MechanicProfile
-
-# Register your models here.
-
-
-admin.site.register(MechanicProfile)
--- backend/mechanics/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import include, path
-from mechanics import views
-
-urlpatterns = [
-    path('list/', views.NearMechanicsListAPIView.as_view()),
-]
